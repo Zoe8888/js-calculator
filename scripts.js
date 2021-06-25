af572a752f2317c851488fdf37adff7bf04f1f43
Zoe <82158799+Zoe8888@users.noreply.github.com>
2021-06-25 10:08:40 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,107 +1,115 @@
-const calculator = document.querySelector('.calculator')
-const display = calculator.querySelector('.display')
-const keys = calculator.querySelector('.keys')
+const calculator = document.querySelector(".calculator");
+const display = calculator.querySelector(".display");
+const keys = calculator.querySelector(".keys");
 
 const calculate = (n1, operator, n2) => {
-  const num1 = parseFloat(n1)
-  const num2 = parseFloat(n2)
-  if (operator === 'add') return num1 + num2
-  if (operator === 'subtract') return num1 - num2
-  if (operator === 'multiply') return num1 * num2
-  if (operator === 'divide') return num1 / num2
-}
+  const num1 = parseFloat(n1);
+  const num2 = parseFloat(n2);
+  if (operator === "add") {
+    return num1 + num2;
+  }
+  if (operator === "subtract") {
+    return num1 - num2;
+  }
+  if (operator === "multiply") {
+    return num1 * num2;
+  }
+  if (operator === "divide") {
+    return num1 / num2;
+  }
+};
 
-const getKeyType = key => {
-  const { action } = key.dataset
-  if (!action) return 'number'
+const getKeyType = (key) => {
+  const { action } = key.dataset;
+  if (!action) {
+    return "number";
+  }
   if (
-    action === 'add' ||
-    action === 'subtract' ||
-    action === 'multiply' |
-    action === 'divide'
-  ) return 'operator'
-  return action
-}
+    action === "add" ||
+    action === "subtract" ||
+    (action === "multiply") | (action === "divide")
+  ) {
+    return "operator";
+  }
+  {
+    return action;
+  }
+};
 
 const result = (key, displayedNum, state) => {
-  const keyContent = key.textContent
-  const keyType = getKeyType(key)
-  const {
-    firstValue,
-    operator,
-    modValue,
-    previousKeyType
-  } = state
+  const keyContent = key.textContent;
+  const keyType = getKeyType(key);
+  const { firstValue, operator, modValue, previousKeyType } = state;
 
-  if (keyType === 'number') {
-    return displayedNum === '0' ||
-      previousKeyType === 'operator' ||
-      previousKeyType === 'calculate'
+  if (keyType === "number") {
+    return displayedNum === "0" ||
+      previousKeyType === "operator" ||
+      previousKeyType === "calculate"
       ? keyContent
-      : displayedNum + keyContent
+      : displayedNum + keyContent;
   }
 
-  if (keyType === 'operator') {
+  if (keyType === "operator") {
     return firstValue &&
       operator &&
-      previousKeyType !== 'operator' &&
-      previousKeyType !== 'calculate'
+      previousKeyType !== "operator" &&
+      previousKeyType !== "calculate"
       ? calculate(firstValue, operator, displayedNum)
-      : displayedNum
+      : displayedNum;
   }
 
-  if (keyType === 'clear') return 0
+  if (keyType === "clear") {
+    return 0;
+  }
 
-  if (keyType === 'calculate') {
+  if (keyType === "calculate") {
     return firstValue
-      ? previousKeyType === 'calculate'
+      ? previousKeyType === "calculate"
         ? calculate(displayedNum, operator, modValue)
         : calculate(firstValue, operator, displayedNum)
-      : displayedNum
+      : displayedNum;
   }
-}
+};
 
 const updateCalculation = (key, calculator, calculatedValue, displayedNum) => {
-  const keyType = getKeyType(key)
-  const {
-    firstValue,
-    operator,
-    modValue,
-    previousKeyType
-  } = calculator.dataset
+  const keyType = getKeyType(key);
+  const { firstValue, operator, modValue, previousKeyType } =
+    calculator.dataset;
 
-  calculator.dataset.previousKeyType = keyType
+  calculator.dataset.previousKeyType = keyType;
 
-  if (keyType === 'operator') {
-    calculator.dataset.operator = key.dataset.action
-    calculator.dataset.firstValue = firstValue &&
+  if (keyType === "operator") {
+    calculator.dataset.operator = key.dataset.action;
+    calculator.dataset.firstValue =
+      firstValue &&
       operator &&
-      previousKeyType !== 'operator' &&
-      previousKeyType !== 'calculate'
-      ? calculatedValue
-      : displayedNum
+      previousKeyType !== "operator" &&
+      previousKeyType !== "calculate"
+        ? calculatedValue
+        : displayedNum;
   }
 
-  if (keyType === 'calculate') {
-    calculator.dataset.modValue = firstValue && previousKeyType === 'calculate'
-      ? modValue
-      : displayedNum
+  if (keyType === "calculate") {
+    calculator.dataset.modValue =
+      firstValue && previousKeyType === "calculate" ? modValue : displayedNum;
   }
 
-  if (keyType === 'clear') {
-    calculator.dataset.firstValue = ''
-    calculator.dataset.modValue = ''
-    calculator.dataset.operator = ''
-    calculator.dataset.previousKeyType = ''
+  if (keyType === "clear") {
+    calculator.dataset.firstValue = "";
+    calculator.dataset.modValue = "";
+    calculator.dataset.operator = "";
+    calculator.dataset.previousKeyType = "";
   }
-}
+};
 
-keys.addEventListener('click', e => {
-  if (!e.target.matches('button')) return
-  const key = e.target
-  const displayedNum = display.textContent
-  const resultString = result(key, displayedNum, calculator.dataset)
+keys.addEventListener("click", (e) => {
+  if (!e.target.matches("button")) {
+    return;
+  }
+  const key = e.target;
+  const displayedNum = display.textContent;
+  const resultString = result(key, displayedNum, calculator.dataset);
 
-  display.textContent = resultString
-  updateCalculation(key, calculator, resultString, displayedNum)
-})
+  display.textContent = resultString;
+  updateCalculation(key, calculator, resultString, displayedNum);
+});
